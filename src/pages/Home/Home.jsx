34409d763484e821ec4ddfecd057dd0b6a837be8
Maxime Esteves <maxime.esteves81@orange.fr>
2026-01-30 13:07:30 +0100

    update profil

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,19 +25,19 @@ export default function Home() {
     <div className="home-container">
       <Text
         title="Développeur front-end"
-        description="👋 Bonjour, je suis <strong>Maxime Esteves</strong>, en reconversion professionnelle, titulaire d'un diplome Développeur Web, je recherche une entreprise pour commencer ma nouvelle vie professionnelle en CDD/CDI ou alternance. <br /> <br /> Je suis curieux et envieux d'apprentissage ! Découvrez mon parcours et mes différents projets ☺️"
+        description="👋 Bonjour, je suis <strong>Maxime Esteves</strong>, développeur web diplômé, je mets mes compétences en front-end au service de projets digitaux ambitieux. Rigoureux et motivé, je souhaite intégrer une entreprise afin de contribuer activement à son développement digital. ☺️"
       />
       <Projet showAll={showAll} setShowAll={setShowAll} />
       <Text
         title="Mes skills"
-        description="💪 Titulaire d’un titre professionnel de Développeur Web, je bénéficie également de sept années d’expérience dans la logistique au sein d’un commerce de gros, ainsi que d'une expérience en vente et bureau d’études en électricité. <br> Ce parcours m’a permis de développer des compétences transversales solides, reflétant ma persévérance, mon adaptabilité et mon engagement dans chaque projet entrepris. <br> <br> Je suis autodidacte, curieux et envieux d'apprentissage ! Découvre mes skills ☺️"
+        description="💪 Titulaire d’un titre professionnel de Développeur Web, je bénéficie également de sept années d’expérience dans la logistique au sein d’un commerce de gros, ainsi que d'une expérience en vente et bureau d’études en électricité. <br> Ce parcours m’a permis de développer des compétences transversales solides, reflétant ma persévérance, mon adaptabilité et mon engagement dans chaque projet entrepris. <br> <br> Je suis autonome, curieux et envieux d'apprentissage ! Découvre mes skills ☺️"
       />
       <Skills />
       <div className="contact-wrapper" id="contact">
         <Text
           title="Contact"
-          description="💪 Ma curiosité et mon engagement dans chacune des missions que j'ai menées dans ma carrière et mes projets m’ont motivé aujourd'hui à me lancer dans cette reconversion professionnelle. 
-          <br><br>Je souhaite, en intégrant une alternance, évoluer dans ce métier en me confrontant directement au monde du travail, en parallèle du programme scolaire. <br> En tant qu'employer en CCD ou CDI je souhaite apporter toute ma motivation et mes compétences au service de l'entreprise.
+          description="💪 Ma curiosité et mon engagement dans chacune des missions que j'ai menées dans ma carrière et mes réalisations m’ont motivé aujourd'hui à me lancer dans cette reconversion professionnelle. 
+          <br>
         
         <br><br>Je suis disponible dans la zone de 📍 Montauban (82) 📍 Toulouse (31), mais également prêt à travailler en 🖥️ 100 % distanciel ! ☺️"
         />
